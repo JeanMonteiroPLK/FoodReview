19e4f888ced9b573a99c5ed86dc7ae2a93f3e6a7
EquipeIV <equipeivsa@gmail.com>
2022-08-10 19:19:18 -0300

    Config DB.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : 'dev142021',
+  database : 'foodreview'
 });
  
 connection.connect();
